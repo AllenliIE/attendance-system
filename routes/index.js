7d887a98a55c4d8d40a779545ee1ac4d4f6d8e0a
AllenliIE <allen.li.service@gmail.com>
2023-01-09 17:17:32 +0800

    feat: add signup page

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const attendController = require("../controllers/attendance-controller");
+const userController = require('../controllers/user-controller') 
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get("/attendance", attendController.getAttendance);
 
 router.use("/", (req, res) => res.redirect("/attendance"));
