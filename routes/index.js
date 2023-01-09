fbb6fe6fb26046a83c90e397fba95783ff1a8218
AllenliIE <allen.li.service@gmail.com>
2023-01-09 17:36:35 +0800

    feat: passport init & signin

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const passport = require('../config/passport')
 const attendController = require("../controllers/attendance-controller");
 const userController = require('../controllers/user-controller') 
 const { generalErrorHandler } = require('../middleware/error-handler')
@@ -9,6 +9,9 @@ const admin = require('./modules/admin')
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
+router.get('/logout', userController.logout)
 router.get("/attendance", attendController.getAttendance);
 router.use("/", (req, res) => res.redirect("/attendance"));
 router.use('/', generalErrorHandler)
