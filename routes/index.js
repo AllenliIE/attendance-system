a42c2c973bf89c051486ce571fba82b2c75195b2
AllenliIE <allen.li.service@gmail.com>
2023-01-09 17:53:46 +0800

    feat: user model add is_admin

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const passport = require('../config/passport')
 const attendController = require("../controllers/attendance-controller");
 const userController = require('../controllers/user-controller') 
 const { generalErrorHandler } = require('../middleware/error-handler')
+const { authenticated } = require('../middleware/auth') 
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
@@ -12,7 +13,7 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
 router.get('/logout', userController.logout)
-router.get("/attendance", attendController.getAttendance);
+router.get("/attendance", authenticated, attendController.getAttendance);
 router.use("/", (req, res) => res.redirect("/attendance"));
 router.use('/', generalErrorHandler)
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+const { authenticatedAdmin } = require('../../middleware/auth') 
 
-router.get('/attendance', adminController.getAttendance)
+router.get('/attendance', authenticatedAdmin, adminController.getAttendance)
 router.use('/', (req, res) => res.redirect('/admin/attendance'))
 
 module.exports = router
